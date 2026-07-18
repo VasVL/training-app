1bcdf9c1be4907bc5d11ea709e65dc55bdc4272c
VasVL <sobakagman.com@gmail.com>
2026-07-18 19:55:03 +0300

    Initial commit: basic Android project structure

---
+++ Android/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TrainingApp"
+
+include(":app")
+
+// Feature modules will be included here via:
+// include(":features:<name>")
